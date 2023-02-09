38e5c524e1ba20f2fb4a6b2ed37080cd7ce3cf87
Adrian Sivilha <108024375+adrian-siv@users.noreply.github.com>
2023-02-09 16:19:14 -0300

    Update all
    Add feature "trailer".

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var listaImagemFilmes = ["https://img.elo7.com.br/product/zoom/1E0EB9C/poster-cartaz-o-poderoso-chefao-chefao.jpg", "https://br.web.img3.acsta.net/pictures/16/10/06/23/05/054278.jpg", "https://waltermattos.com/site2/wp-content/uploads/2014/11/tut_Analise_Grafica_Cartaz_Interstellar_05_011.png", "https://br.web.img3.acsta.net/medias/nmedia/18/87/02/76/19871246.jpg", "https://i.pinimg.com/474x/b4/25/97/b4259768165c3d8d8365d9c495b507e2.jpg"];
 var listaNomeFilmes = ["O Poderoso Chefão", "A Chegada", "Interestelar", "Cidade de Deus", "Sussurros do Coração"]
 
-var listaTrailers = ["https://www.youtube.com/watch?v=SaHZHU-44XA", "https://www.youtube.com/watch?v=rNciXGzYZms", "https://www.youtube.com/watch?v=BYUZhddDbdc", "https://www.youtube.com/watch?v=nBWtTrLxUjM", "https://www.youtube.com/watch?v=0pVkiod6V0U"]
+var listaTrailerFilmes = ["https://www.youtube.com/watch?v=SaHZHU-44XA", "https://www.youtube.com/watch?v=rNciXGzYZms", "https://www.youtube.com/watch?v=BYUZhddDbdc", "https://www.youtube.com/watch?v=nBWtTrLxUjM", "https://www.youtube.com/watch?v=0pVkiod6V0U"]
 
 document.write("<div id='container-todos-filmes'>")
 
@@ -9,7 +9,7 @@ for (i = 0; i < listaImagemFilmes.length; i++) {
     document.write(`<div id='container-filme-${i + 1}' class='container-filme'>`)
     document.write(`<img src =${listaImagemFilmes[i]}>`);
     document.write(`<p class='nomes-filmes'>${listaNomeFilmes[i]}</p>`);
-    document.write(`<button class='ver-trailer'><a href="${listaTrailers[i]}" target="_blank">Ver Trailer</a></button>`)
+    document.write(`<p class='ver-trailer'><a href="${listaTrailerFilmes[i]}" target="_blank">Ver Trailer</a></p>`)
     document.write("</div>");
 }
 document.write("</div>")
@@ -26,6 +26,7 @@ function verificaLink() {
 function inserirFilme() {
         var nomeFilme = document.getElementById("nome-filme").value;
         var imagemFilme = document.getElementById("imagem-filme").value;
+        var trailerFilme = document.getElementById("trailer-filme").value;
         var filmeRepetido = false;
 
         for (j = 0; j < listaNomeFilmes.length; j++) {
@@ -39,6 +40,7 @@ function inserirFilme() {
         if (filmeRepetido == false && verificaLink()) {
             listaNomeFilmes.push(nomeFilme);
             listaImagemFilmes.push(imagemFilme);
+            listaTrailerFilmes.push(trailerFilme);
 
             var containerTodosFilmes = document.getElementById("container-todos-filmes");
 
@@ -48,7 +50,7 @@ function inserirFilme() {
 
             containerFilme.innerHTML = `<img src=${imagemFilme}>`;
             containerFilme.innerHTML = containerFilme.innerHTML + `<p class="nomes-filmes">${nomeFilme}</p>`;
-            containerFilme.innerHTML = containerFilme.innerHTML + `<button class="nomes-filmes">${nomeFilme}</button>`;
+            containerFilme.innerHTML = containerFilme.innerHTML + `<p class='ver-trailer'><a href="${listaTrailerFilmes[i]}" target="_blank">Ver Trailer</a></p>`;
             
 
             i++;
@@ -56,5 +58,7 @@ function inserirFilme() {
 
         document.getElementById("nome-filme").value = "";
         document.getElementById("imagem-filme").value = "";
+        document.getElementById("trailer-filme").value = "";
+        
 
 }
