fd9c6ee859fe6546894b5506e718d68ba791a64e
Adrian Sivilha <108024375+adrian-siv@users.noreply.github.com>
2023-02-16 17:32:11 -0300

    Update all
    Try adding pop-up feature to highlight videos.

--- main.js
+++ main.js
@@ -90,3 +90,14 @@ function removerJogador() {
 }
 removerJogador();
 
+$(document).ready(function (){
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+    });
+});
