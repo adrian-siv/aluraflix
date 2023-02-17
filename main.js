922b34bbf54232c2104b22af5a94ed0d61c3ad64
Adrian Sivilha <108024375+adrian-siv@users.noreply.github.com>
2023-02-17 12:25:14 -0300

    Update all
    Cancel pop-up feature to highlight videos, because of Youtube copyright rules.

--- main.js
+++ main.js
@@ -89,15 +89,3 @@ function removerJogador() {
     }
 }
 removerJogador();
-
-$(document).ready(function() {
-	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-
-		fixedContentPos: false
-	});
-});
